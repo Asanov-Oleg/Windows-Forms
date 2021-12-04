9888c1db8b4febb9f19fd69860c8887fc3434bd8
Asanov-Oleg <whitehanded_liar@inbox.ru>
2021-12-04 10:34:42 +0300

    Добавил три кнопки, заменил текст формы, изменил свойства Size, Location, Name и Text у кнопок

--- WinAnim/Form1.Designer.cs
+++ WinAnim/Form1.Designer.cs
@@ -29,13 +29,57 @@ namespace WinAnim
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAW_BLEND = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAW_BLEND
+            // 
+            this.btnAW_BLEND.Location = new System.Drawing.Point(30, 62);
+            this.btnAW_BLEND.Name = "btnAW_BLEND";
+            this.btnAW_BLEND.Size = new System.Drawing.Size(232, 23);
+            this.btnAW_BLEND.TabIndex = 0;
+            this.btnAW_BLEND.Text = "Проявление";
+            this.btnAW_BLEND.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Горизонтальное появление";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(232, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Появление из центра";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnAW_BLEND);
+            this.Name = "Form1";
+            this.Text = "Анимация формы";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAW_BLEND;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
