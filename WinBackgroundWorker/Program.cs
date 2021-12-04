3130bdde92559b9d5e8e944b5023855ed335f1fe
Asanov-Oleg <whitehanded_liar@inbox.ru>
2021-12-04 12:30:37 +0300

    taskfault

--- WinBackgroundWorker/Program.cs
+++ WinBackgroundWorker/Program.cs
@@ -8,9 +8,6 @@ namespace WinBackgroundWorker
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
