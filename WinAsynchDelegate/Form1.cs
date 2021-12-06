e685eb6d59d3e30c5bb32951ca5f03381d6afea0
Asanov-Oleg <whitehanded_liar@inbox.ru>
2021-12-06 16:18:32 +0300

    Добавил сообщающий о продвижении операции метод, который устанавливает значение элемента управления ProgressBar потокобезопасным способом, и делегата этого метода. Дополнил цикл For метода TimeConsumingMethod, добавил логическую переменную Cancel, также добавил код после цикла For метода TimeConsumingMethod

--- WinAsynchDelegate/Form1.cs
+++ WinAsynchDelegate/Form1.cs
@@ -25,13 +25,42 @@ namespace WinAsynchDelegate
                 MessageBox.Show("Поле должно содержать цифры");
             }
         }
+        bool Cancel;
 
         private void TimeConsumingMethod(int seconds)
         {
             for (int j = 1; j <= seconds; j++)
+            {
+                SetProgress((int)(j * 100) / seconds);
                 System.Threading.Thread.Sleep(1000);
+                if (Cancel)
+                    break;
+            }
+            if (Cancel)
+            {
+                System.Windows.Forms.MessageBox.Show("Cancelled");
+                Cancel = false;
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Complete");
+            }
         }
 
         private delegate void TimeConsumingMethodDelegate(int seconds);
+
+        public delegate void SetProgressDelegate(int val);
+        public void SetProgress(int val)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+            else
+            {
+                progressBar1.Value = val;
+            }
+        }
     }
 }
