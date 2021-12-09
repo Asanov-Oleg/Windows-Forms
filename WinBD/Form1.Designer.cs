f8c9faaec2543b3e8b8df135a07137994b625898
Asanov-Oleg <whitehanded_liar@inbox.ru>
2021-12-09 16:06:28 +0300

    taskfault4

--- WinBD/Form1.Designer.cs
+++ WinBD/Form1.Designer.cs
@@ -34,6 +34,15 @@ namespace WinBD
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.rbProductDataSet1 = new WinBD.RBProductDataSet();
+            this.товарыTableAdapter1 = new WinBD.RBProductDataSet1TableAdapters.ТоварыTableAdapter();
+            this.PstvtextBox = new System.Windows.Forms.TextBox();
+            this.AdrstextBox = new System.Windows.Forms.TextBox();
+            this.CodetextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.rbProductDataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // oleDbCommand1
@@ -48,7 +57,7 @@ namespace WinBD
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(78, 62);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(161, 52);
             this.button1.TabIndex = 0;
@@ -67,7 +76,7 @@ namespace WinBD
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(78, 160);
+            this.button2.Location = new System.Drawing.Point(12, 70);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(161, 49);
             this.button2.TabIndex = 2;
@@ -75,17 +84,82 @@ namespace WinBD
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // rbProductDataSet1
+            // 
+            this.rbProductDataSet1.DataSetName = "RBProductDataSet";
+            this.rbProductDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // товарыTableAdapter1
+            // 
+            this.товарыTableAdapter1.ClearBeforeFill = true;
+            // 
+            // PstvtextBox
+            // 
+            this.PstvtextBox.Location = new System.Drawing.Point(170, 199);
+            this.PstvtextBox.Name = "PstvtextBox";
+            this.PstvtextBox.Size = new System.Drawing.Size(100, 20);
+            this.PstvtextBox.TabIndex = 3;
+            // 
+            // AdrstextBox
+            // 
+            this.AdrstextBox.Location = new System.Drawing.Point(170, 225);
+            this.AdrstextBox.Name = "AdrstextBox";
+            this.AdrstextBox.Size = new System.Drawing.Size(100, 20);
+            this.AdrstextBox.TabIndex = 4;
+            // 
+            // CodetextBox
+            // 
+            this.CodetextBox.Location = new System.Drawing.Point(170, 251);
+            this.CodetextBox.Name = "CodetextBox";
+            this.CodetextBox.Size = new System.Drawing.Size(100, 20);
+            this.CodetextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 202);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Поставщик";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Адрес поставщика";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Код поставщика";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CodetextBox);
+            this.Controls.Add(this.AdrstextBox);
+            this.Controls.Add(this.PstvtextBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.rbProductDataSet1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -96,6 +170,14 @@ namespace WinBD
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
+        private RBProductDataSet rbProductDataSet1;
+        private RBProductDataSet1TableAdapters.ТоварыTableAdapter товарыTableAdapter1;
+        private System.Windows.Forms.TextBox PstvtextBox;
+        private System.Windows.Forms.TextBox AdrstextBox;
+        private System.Windows.Forms.TextBox CodetextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
